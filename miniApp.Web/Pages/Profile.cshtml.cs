63a848432b3ce63e1f48c2c38c3d4dd2615b10a7
@Arnan-TiTo <arnan.tp@gmail.com>
2025-06-27 05:36:35 +0700

    Add UI

---
+++ miniApp.Web/Pages/Profile.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace miniApp.Web.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public string Username { get; set; } = "bb";
+        public string RoleDescription { get; set; } = "Staff";
+        public string Email { get; set; } = "bb@minichic.com";
+        public string Phone { get; set; } = "0812345678";
+
+        public void OnGet()
+        {
+            // ดึงข้อมูลจริงจาก session หรือ API ได้
+        }
+    }
+}
---
+++ miniApp.Web/Pages/Shared/_FooterMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace miniApp.Web.Pages.Shared
+{
+    public class _FooterMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
